3997bdae2e8a857154f0aa86ab4cc6a10c4d9f3c
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-03 23:59:04 -0300

    feat: add kakfa broker

--- internal/infra/akafka/kafka.go
+++ internal/infra/akafka/kafka.go
@@ -1,1 +1,23 @@
 package akafka
+
+import "github.com/confluentinc/confluent-kafka-go/v2/kafka"
+
+func Consume(topics []string, servers string, msgChan chan *kafka.Message) {
+	kafkaConsumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": servers,
+		"group.id":          "imersao",
+		"auto.offser.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	kafkaConsumer.SubscribeTopics(topics, nil)
+	for {
+		msg, err := kafkaConsumer.ReadMessage(-1)
+		if err == nil {
+			msgChan <- msg
+		}
+	}
+}
